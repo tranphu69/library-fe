ed93334d3da973509fe95581329ec1d993780d36
Trần Ngọc Phú <phu.tran@lifesup.local>
2025-11-18 09:34:02 +0700

    push-againt

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('library-fe');
+  protected readonly title = signal('library-fee');
 }
