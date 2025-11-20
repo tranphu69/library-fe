fd57e4fcdf386410ba03bab816cac7e0f3509a95
Trần Ngọc Phú <phu.tran@lifesup.local>
2025-11-20 16:59:46 +0700

    base-design-role

---
+++ src/app/features/role/component/list-table/list-table.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-table',
+  imports: [],
+  templateUrl: './list-table.html',
+  styleUrl: './list-table.css',
+})
+export class ListTable {
+
+}
